1f2abefb02c13b17c0d0a38ddddaf527484376f8
raphaeldjangmah <rdosro-agbo@st.ug.edu.gh>
2022-12-06 16:31:11 +0000

    welcome page

--- test1.php
+++ test1.php
@@ -7,15 +7,3 @@ $date_b = date('m/d/Y h:i:s a',time());
 echo time() - 1670336093;
 
 return; 
-
-
-  if($_SERVER['REQUEST_METHOD'] == 'GET'){
-    echo "ACCESS DENIED";
-  }else{
-    if(isset($_GET['name'])){
-       echo ($_GET['name']);
-    }else{
-        echo "POST -> not seen";
-    }
-  }
-
