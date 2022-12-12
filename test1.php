0351b61caa8471eb0484a08459dbba67d5a6e785
raphaeldjangmah <rdosro-agbo@st.ug.edu.gh>
2022-12-12 16:01:58 +0000

    mild upgrade made

--- test1.php
+++ test1.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html>
-<body>
+<?php
 
-<p>Click on the button to copy the text from the text field. Try to paste the text (e.g. ctrl+v) afterwards in a different window, to see the effect.</p>
+$code = "dasilva";
+$code = "mista";
+$password   = password_hash($code, PASSWORD_BCRYPT);
 
-<input type="text" value="Hello World" id="myInput">
-<button onclick="myFunction()">Copy text</button>
-
-<script>
-function myFunction() {
-  // Get the text field
-  var copyText = document.getElementById("myInput");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-  
-  // Alert the copied text
-  alert("COPIED! " + copyText.value);
-}
-</script>
-
-</body>
-</html>
+echo $password;
